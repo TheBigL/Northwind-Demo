13bb7620b77f3936d9f35251e2b3589d4f6793f4
TheBigL <lmohamed1@studentmail.nait.ca>
2016-10-19 22:03:46 -0600

    NuGetPackages Updating

---
+++ Northwind Demo/Northwind Data/Security/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind_Data.Security
+{
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
---
+++ Northwind Demo/Northwind Data/Security/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind_Data.Security
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int? EmployeeId { get; set; }
+        public string CustomerId { get; set; }
+    }
+}
